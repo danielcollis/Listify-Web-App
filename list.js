aacd99013eea7996d7348eb9de3e4d603eb2b6dc
danielcollis <daniel.collis13@gmail.com>
2025-03-19 09:19:02 -0400

    added a side menu
    added a hamburger icon with a home button to navigate home

--- list.js
+++ list.js
@@ -74,3 +74,13 @@ document.getElementById('priceInput').addEventListener('keypress', function(even
         addLink();
     }
 });
+
+// Toggle side menu when hamburger icon is clicked
+document.getElementById("menuToggle").addEventListener("click", function() {
+    let menu = document.getElementById("sideMenu");
+    if (menu.style.width === "250px") {
+        menu.style.width = "0"; // Close the menu
+    } else {
+        menu.style.width = "250px"; // Open the menu
+    }
+});
