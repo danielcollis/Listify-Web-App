3ea79ca0d4d26a3986873a6560ab64283dc8c76a
danielcollis <daniel.collis13@gmail.com>
2025-03-24 08:26:12 -0400

    added create list button to home page

---
+++ home.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("new-list").addEventListener("click", function() {
+        window.location.href = "list.html";
+    });
+});
